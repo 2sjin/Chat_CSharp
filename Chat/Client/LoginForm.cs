8f0d5ded3c1fee50f419e0d3d58bd695f2d7abf3
options3224@gmail.com <options3224@gmail.com>
2023-04-04 16:59:16 +0900

    로그인 버튼 클릭 이벤트 핸들러 비동기식으로 변경(async)

--- Chat/Client/LoginForm.cs
+++ Chat/Client/LoginForm.cs
@@ -5,12 +5,14 @@ namespace Client {
         }
 
         // [로그인] 버튼 클릭
-        private void btnLogin_Click(object sender, EventArgs e) {
+        private async void btnLogin_Click(object sender, EventArgs e) {
             // 텍스트박스 빈칸 확인
             if (string.IsNullOrEmpty(tbID.Text) || string.IsNullOrEmpty(tbNick.Text)) {
                 MessageBox.Show("ID와 닉네임을 모두 입력하세요.", this.Text);
                 return;
             }
+
+            
         }
     }
 }
